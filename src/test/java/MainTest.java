9a9dbd04157ab8da2f9a38b1fcfa61a3136b0550
Grzegorz Szczurek <gszczure@student.wszib.edu.pl>
2025-12-06 18:45:56 +0100

    feat: Add dummy test and CI workflow

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void dummyTrueTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void dummyFalseTest() {
+        assertFalse(false);
+    }
+}
